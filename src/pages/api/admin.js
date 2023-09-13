b97f94a32784d9e3f0a615b192a91f4fc0d4fb60
AnBoyvan <andrew.boyvan1988@gmail.com>
2023-09-13 18:52:34 +0300

    Update admin.js

--- src/pages/api/admin.js
+++ src/pages/api/admin.js
@@ -22,10 +22,9 @@ export default async function handler(req, res) {
       .status(200)
       .json({ message: 'Дані оновлено успішно.' });
   } catch (error) {
-    return error;
-    // .status(500)
-    // .json({ error: 'Помилка при оновленні даних.' });
-
-    // .json({ error: 'Помилка при оновленні даних.' });
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: 'Помилка при оновленні даних.' });
   }
 }
